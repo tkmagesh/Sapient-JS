43effc08ebe46d6d27ca444c92b88e981e4bde23
Magesh <tkmagesh77@gmail.com>
2013-08-05 11:53:57 +0530

    Namespace Demo

--- Day-4/EventUtils.js
+++ Day-4/EventUtils.js
@@ -1,29 +1,34 @@
-var eventUtils = {
-	addEvent : function (node,eventName,fn){
-	  if (node.addEventListener){
-	    node.addEventListener(eventName,fn);
-	  }
-	  else if (node.attachEvent){
-	    node.attachEvent("on" + eventName,fn)
-	  }
-	},
+var myApp = myApp || {};
+(function(){
+	var eventUtils = {
+		addEvent : function (node,eventName,fn){
+		  if (node.addEventListener){
+		    node.addEventListener(eventName,fn);
+		  }
+		  else if (node.attachEvent){
+		    node.attachEvent("on" + eventName,fn)
+		  }
+		},
 
-	removeEvent : function(node,eventName,fn){
-	  if (node.removeEventListener){
-	    node.removeEventListener(eventName,fn);
-	  }
-	  else if (node.detachEvent){
-	    node.detachEvent("on" + eventName,fn)
-	  }
-	},
+		removeEvent : function(node,eventName,fn){
+		  if (node.removeEventListener){
+		    node.removeEventListener(eventName,fn);
+		  }
+		  else if (node.detachEvent){
+		    node.detachEvent("on" + eventName,fn)
+		  }
+		},
 
-	getEventTarget : function(evtObj){
-		if (evtObj.srcElement) return evtObj.srcElement;
-		return evtObj.target;
-	},
+		getEventTarget : function(evtObj){
+			if (evtObj.srcElement) return evtObj.srcElement;
+			return evtObj.target;
+		},
 
-	stopEvent : function(evtObj){
-		if (evtObj.stopPropagation) evtObj.stopPropagation();
-		evtObj.cancelBubble = true;
-	}
-}
+		stopEvent : function(evtObj){
+			if (evtObj.stopPropagation) evtObj.stopPropagation();
+			evtObj.cancelBubble = true;
+		}
+	};
+	myApp.eventUtils = eventUtils;
+})();
+
---
+++ Day-4/stringUtils.js
@@ -0,0 +1,9 @@
+var myApp = myApp || {};
+(function(){
+	var stringUtils = {
+		someHelper : function(){
+
+		}
+	};
+	myApp.stringUtils = stringUtils
+})();
