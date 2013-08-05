98705d07211da40c53b298ab5ae09065f251ecbc
Magesh <tkmagesh77@gmail.com>
2013-08-05 11:18:34 +0530

    Modern Events

---
+++ Day-4/EventUtils.js
@@ -0,0 +1,29 @@
+var eventUtils = {
+	addEvent : function (node,eventName,fn){
+	  if (node.addEventListener){
+	    node.addEventListener(eventName,fn);
+	  }
+	  else if (node.attachEvent){
+	    node.attachEvent("on" + eventName,fn)
+	  }
+	},
+
+	removeEvent : function(node,eventName,fn){
+	  if (node.removeEventListener){
+	    node.removeEventListener(eventName,fn);
+	  }
+	  else if (node.detachEvent){
+	    node.detachEvent("on" + eventName,fn)
+	  }
+	},
+
+	getEventTarget : function(evtObj){
+		if (evtObj.srcElement) return evtObj.srcElement;
+		return evtObj.target;
+	},
+
+	stopEvent : function(evtObj){
+		if (evtObj.stopPropagation) evtObj.stopPropagation();
+		evtObj.cancelBubble = true;
+	}
+}
